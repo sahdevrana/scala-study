ba0e98d6f70c60643b868cb9fd78c573f24edc39
Sahdev Rana <kumarsahdev32@gmail.com>
2019-09-04 17:02:21 +0530

    Added Extractor example using companion object and class.

--- src/sahdev/learning/scala/extractor/ExtractorExample_Companion.scala
+++ src/sahdev/learning/scala/extractor/ExtractorExample_Companion.scala
@@ -1,14 +1,16 @@
 package sahdev.learning.scala.extractor
 
  class ApplyMethodExample {
+
+ // def myFun:String="strinhg"
   
   def apply(email:String,domain:String):String=
   {
    // return email+"@"+domain
-    return s"${email}@+${domain}"
+    return s"${email}@${domain}"
   }
   
- // def myFun:String="strinhg"
+  //ApplyMethodExample("Sahdev","gmail.com") // If I will create apply method in object then I can access that by using object name with ().
   
   def unapply(emailId:String):Option[(String,String)]=
   {
@@ -25,24 +27,28 @@ package sahdev.learning.scala.extractor
     
   }
   
-  
 }
 
 object ApplyMethodExample
 {
   
-    
   def main(args: Array[String]): Unit = {
     
-    val ex=new ApplyMethodExample()
+    val ex=new ApplyMethodExample() //Created an object of comapnion class.
     
-//    ex.apply("gg", "kk")
+//    ex.apply("gg", "kk") //I can call apply method like this also and it will execute. 
 //    ex.myFun
-   
+  
     
-    val emailadd=ex("Sahdev","gmail.com")//Apply method called automatically once you put brackets and argument(can be zero or more) in an object ex.apply("Dibyajita","@gmail.com")
+    val emailadd=ex("Sahdev","gmail.com") //Apply method called automatically once you put brackets and argument(can be zero or more) in an object ex.apply("Sahdev","@gmail.com")
     
-    println(ex.unapply(emailadd).get._1)
+    println(ex.unapply(emailadd).get._1) // I can call unapply method like this also.
+    
+    emailadd match {
+      case ex(splitedEmailID) => println(s"${splitedEmailID._1} is having an accoung on ${splitedEmailID._2} domain.")
+      case _ => println("This is my default case.")
+    }
+    
     }
     
   }
